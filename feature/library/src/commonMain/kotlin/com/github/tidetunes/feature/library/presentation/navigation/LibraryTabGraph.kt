10f4ff78956e4c52cf91e2d647f97cca2584b7a3
JulyStar-Lv <30335624+JulyStar-Lv@users.noreply.github.com>
2026-07-05 15:43:50 +0800

    Pass now playing callback to library

--- feature/library/src/commonMain/kotlin/com/github/tidetunes/feature/library/presentation/navigation/LibraryTabGraph.kt
+++ feature/library/src/commonMain/kotlin/com/github/tidetunes/feature/library/presentation/navigation/LibraryTabGraph.kt
@@ -1,22 +1,24 @@
 package com.github.tidetunes.feature.library.presentation.navigation
 
-import com.github.tidetunes.feature.library.presentation.LibraryRoot
-
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.github.tidetunes.feature.library.presentation.LibraryRoot
 
 @Composable
 fun LibraryTabGraph(
     navController: NavHostController,
+    onOpenNowPlaying: () -> Unit = {},
 ) {
     NavHost(
         navController = navController,
         startDestination = "library",
     ) {
         composable("library") {
-            LibraryRoot()
+            LibraryRoot(
+                onOpenNowPlaying = onOpenNowPlaying,
+            )
         }
     }
 }
